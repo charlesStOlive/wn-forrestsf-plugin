588276f052a0217ad0662fcbc959af1254f5999a
Charles <charles@notilac.fr>
2023-10-10 12:01:28 +0200

    MAJ
    Avec langue

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,15 +1,20 @@
 <?php
 
 return [
+    'controllers' => [
+        'logsfs' => [
+            'label' => 'Log Sfs',
+        ],
+    ],
     'global' => [
         'authorize' => 'Autoriser',
         'authorize_button' => 'Connecter',
         'authorize_comment' => 'Commentaires autorisations',
-        'authorized' => 'SalesForce est connecté ! ',
-        'authorized_comment' => 'Vous pouvez vous synchroniser ',
+        'authorized' => 'SalesForce est connecté !',
+        'authorized_comment' => 'Vous pouvez vous synchroniser',
         'error' => 'Erreur',
         'error_comment' => 'Commentaire erreur',
-        'unauthorized' => 'SalesForce n\'est pas connecté ! ',
+        'unauthorized' => 'SalesForce n\'est pas connecté !',
         'unauthorized_comment' => 'Vous devez connecter SalesForce',
     ],
     'job' => [
@@ -17,39 +22,32 @@ return [
     ],
     'logsf' => [
         'errors' => 'Erreurs',
-        'form_name' => 'Gestion du log',
         'import_indicator' => 'Chargement des données en cours',
-        'manual_auto' => 'Forcer Import automatique',
-        'manual_popup' => 'Choisir les imports et des options',
+        'manual_auto' => 'Forcer l\'import automatique',
+        'manual_popup' => 'Choisir les imports et les options',
         'name' => 'Nom de l\'import',
         'nb' => 'Nombre de lignes',
         'nb_updated_rows' => 'Lignes enregistrées',
-        'popup_titile' => 'Rapport',
-        'preview_name' => 'Voir le log',
+        'popup_title' => 'Rapport',
         'query' => 'Requête',
         'sf_total_size' => 'Lignes venant de SalesForce',
     ],
     'menu' => [
-        'category' => 'Syncronisation',
-        'logsf' => 'Log SalesForce ',
-        'logsf_description' => 'Connexion et lancement de syncro manuel',
+        'category' => 'Synchronisation',
+        'logsf' => 'Log SalesForce',
+        'logsf_description' => 'Connexion et lancement de synchro manuelle',
         'settings' => 'Options de SalesForce',
         'settings_description' => 'Option et configuration des imports et exports',
-    ],
-    'settings' => [
-        'active_imports' => 'Choisissez les imports actifs',
-        'exports_authorized' => 'Exports autorisés',
-        'imports_authorized' => 'Imports autorisés',
-        'oldest_date' => 'Date de réinitialisation',
     ],
     'models' => [
         'logsf' => [
             'label' => 'Log Sf',
         ],
     ],
-    'controllers' => [
-        'logsfs' => [
-            'label' => 'Log Sfs',
-        ],
+    'settings' => [
+        'active_imports' => 'Choisissez les imports actifs',
+        'exports_authorized' => 'Exports autorisés',
+        'imports_authorized' => 'Imports autorisés',
+        'oldest_date' => 'Date de réinitialisation',
     ],
 ];
